a0f21d1d8a8b19828dd1503148b69dbb5c0b3d78
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2014-04-03 22:50:44 +0400

    Filter now accepts some parameters

--- src/ru/abusalimov/xposed/lightsensorfilter/LightSensorFilter.java
+++ src/ru/abusalimov/xposed/lightsensorfilter/LightSensorFilter.java
@@ -2,33 +2,52 @@ package ru.abusalimov.xposed.lightsensorfilter;
 
 import android.util.Log;
 
+/**
+ * Filters inadequately high and low lux values using Moving Average filter.
+ * @author Eldar Abusalimov
+ */
 public class LightSensorFilter {
 
-	protected static final int MA_WINDOW_SIZE = 7;
-	protected static final int SEEN_BOGUS_WINDOW_SIZE = MA_WINDOW_SIZE;
+	public static final int DEFAULT_SMOOTH_WINDOW = 7;
+	public static final float DEFAULT_SMOOTH_FIXUP = 1.3f;
 
 	protected static final float HIGH_BOGUS_LUX = 30000.0f;
 	protected static final float LOW_BOGUS_LUX = 0.0f;
 
-	protected static final float AVG_FIXUP_COEF = 1.3f;
-
-	private MovingAverage mMA = new MovingAverage(MA_WINDOW_SIZE);
+	private final MovingAverage mMovingAverage;
 	private int mSeenBogus;
+
+	private final int mSmoothWindow;
+	private final float mSmoothFixup;
+
+	public LightSensorFilter() {
+		this(DEFAULT_SMOOTH_WINDOW, DEFAULT_SMOOTH_FIXUP);
+	}
+
+	/**
+	 * @param smoothWindow window size for smoothing bogus values
+	 * @param smoothFixup coefficient to use to adjust smoothed values
+	 */
+	public LightSensorFilter(int smoothWindow, float smoothFixup) {
+		mSmoothWindow = smoothWindow;
+		mSmoothFixup = smoothFixup;
+		mMovingAverage = new MovingAverage(smoothWindow);
+	}
 
 	public float fixupLuxValue(float lux) {
 		if (Float.compare(lux, HIGH_BOGUS_LUX) == 0) {
 			// it goes crazy and shows 30k lux
-			lux = mMA.getAverage() * AVG_FIXUP_COEF;
+			lux = mMovingAverage.getAverage() * mSmoothFixup;
 			Log.v("LightSensorFilter", "fixup bogus high: " + lux + "lux");
 
 			// sometimes it drops from 30k to zero occasionally,
 			// be ready for fluctuations in the nearest future
-			mSeenBogus = SEEN_BOGUS_WINDOW_SIZE;
+			mSeenBogus = mSmoothWindow;
 		} else if (mSeenBogus != 0) {
 
 			if (Float.compare(lux, LOW_BOGUS_LUX) == 0) {
 				// reports zero within few ticks after 30k: LIAR!11
-				lux = mMA.getAverage() / AVG_FIXUP_COEF;
+				lux = mMovingAverage.getAverage() / mSmoothFixup;
 				Log.v("LightSensorFilter", "fixup bogus low:  " + lux + "lux");
 
 			} else {
@@ -37,8 +56,16 @@ public class LightSensorFilter {
 		}
 
 		// TODO Does it need to be fed back to MA unconditionally?
-		mMA.handleNewValue(lux);
+		mMovingAverage.handleNewValue(lux);
 		return lux;
+	}
+
+	public int getSmoothWindow() {
+		return mSmoothWindow;
+	}
+
+	public float getSmoothFixup() {
+		return mSmoothFixup;
 	}
 
 }
