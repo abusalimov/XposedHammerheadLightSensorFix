4cf4df3f1ee325c6fe007cecb30e41b64b4c09f2
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2014-04-03 21:47:17 +0400

    Filter 30k <-> zero lux drops as well

--- src/ru/abusalimov/xposed/lightsensorfilter/LightSensorFilter.java
+++ src/ru/abusalimov/xposed/lightsensorfilter/LightSensorFilter.java
@@ -9,14 +9,33 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class LightSensorFilter implements IXposedHookLoadPackage {
 
 	protected static final int MA_WINDOW_SIZE = 7;
-	protected static final float BOGUS_LUX_VALUE = 30000.0f;
+	protected static final int SEEN_BOGUS_WINDOW_SIZE = MA_WINDOW_SIZE;
+
+	protected static final float HIGH_BOGUS_LUX = 30000.0f;
+	protected static final float LOW_BOGUS_LUX = 0.0f;
 
 	private MovingAverage mMA = new MovingAverage(MA_WINDOW_SIZE);
+	private int mSeenBogus;
 
 	protected float fixupLuxValue(float lux) {
-		if (Float.compare(lux, BOGUS_LUX_VALUE) == 0) {
+		if (Float.compare(lux, HIGH_BOGUS_LUX) == 0) {
+			// it goes crazy and shows 30k lux
 			lux = mMA.getAverage();
-			Log.v("LightSensorFilter", "fixup bogus with: " + lux + "lux");
+			Log.v("LightSensorFilter", "fixup bogus high: " + lux + "lux");
+
+			// sometimes it drops from 30k to zero occasionally,
+			// be ready for fluctuations in the nearest future
+			mSeenBogus = SEEN_BOGUS_WINDOW_SIZE;
+		} else if (mSeenBogus != 0) {
+
+			if (Float.compare(lux, LOW_BOGUS_LUX) == 0) {
+				// reports zero within few ticks after 30k: LIAR!11
+				lux = mMA.getAverage();
+				Log.v("LightSensorFilter", "fixup bogus low:  " + lux + "lux");
+
+			} else {
+				--mSeenBogus;
+			}
 		}
 
 		// TODO Does it need to be fed back to MA unconditionally?
