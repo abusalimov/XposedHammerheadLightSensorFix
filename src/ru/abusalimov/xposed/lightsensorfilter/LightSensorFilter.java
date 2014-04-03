bcb00e5436ef7820678ae63fe2ece50167431881
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2014-04-03 20:56:42 +0400

    Add a comment with references to the original methods being hooked

--- src/ru/abusalimov/xposed/lightsensorfilter/LightSensorFilter.java
+++ src/ru/abusalimov/xposed/lightsensorfilter/LightSensorFilter.java
@@ -19,12 +19,23 @@ public class LightSensorFilter implements IXposedHookLoadPackage {
 				"dispatchSensorEvent",
 				int.class, float[].class, int.class, long.class,
 
+				// dispatchSensorEvent(int handle, float[] values,
+				//         int inAccuracy, long timestamp)
+				//   (see /core/java/android/hardware/SystemSensorManager.java)
+				// is called from native Receiver::handleEvent(int, int, void *)
+				//   (see /core/jni/android_hardware_SensorManager.cpp)
+				// each time a new value is read from HAL.
+
 				new XC_MethodHook() {
 
 					@Override
 					protected void beforeHookedMethod(MethodHookParam param)
 							throws Throwable {
 						int handle = (int) param.args[0];
+
+						// We probably could perform more reliable checks
+						// for a matching sensor here, but... who cares?
+						// On Hammerhead light sensor seems to have handle 1.
 						if (handle == ALS_HANDLE) {
 							float[] values = (float[]) param.args[1];
 
