89b7ee7dee65d9e12a7e6be697b8ee648b655c5c
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2014-04-03 20:10:36 +0400

    Match proper sensor and log its readings

--- src/ru/abusalimov/xposed/lightsensorfilter/LightSensorFilter.java
+++ src/ru/abusalimov/xposed/lightsensorfilter/LightSensorFilter.java
@@ -1,14 +1,37 @@
 package ru.abusalimov.xposed.lightsensorfilter;
 
+import android.util.Log;
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class LightSensorFilter implements IXposedHookLoadPackage {
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-        XposedBridge.log("Loaded app: " + lpparam.packageName);
+		XposedHelpers.findAndHookMethod(
+				"android.hardware.SystemSensorManager$SensorEventQueue",
+				lpparam.classLoader,
+				"dispatchSensorEvent",
+				int.class, float[].class, int.class, long.class,
+
+				new XC_MethodHook() {
+
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param)
+							throws Throwable {
+						int handle = (int) param.args[0];
+						if (handle == 1) {
+							float[] values = (float[]) param.args[1];
+							float lux = values[0];
+							Log.v("LightSensorFilter", "lux value: " + lux);
+						}
+
+						// invoke the original method
+					}
+
+				});
 	}
 
 }
