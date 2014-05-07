9cf993aacc90cf36713438132f036a39a9fec18a
Eldar Abusalimov <eldar.abusalimov@gmail.com>
2014-05-08 02:24:08 +0400

    (minor) change double -> float as we don't feedback synthetic values anymore

--- src/ru/abusalimov/xposed/lightsensorfilter/LightSensorFilter.java
+++ src/ru/abusalimov/xposed/lightsensorfilter/LightSensorFilter.java
@@ -9,7 +9,7 @@ import android.util.Log;
 public class LightSensorFilter {
 
 	public static final long DEFAULT_SMOOTH_WINDOW = 3000l * 1000 * 1000;
-	public static final double DEFAULT_SPIKE_FIXUP = 1.1;
+	public static final float DEFAULT_SPIKE_FIXUP = 1.1f;
 
 	protected static final float LOW_SPIKE_LUX = 0.0f;
 	protected static final float LOW_REPLACEMENT_LUX = 1.0f;
@@ -19,7 +19,7 @@ public class LightSensorFilter {
 	private static final String LOG_TAG = LightSensorFilter.class.getSimpleName();
 
 	private final long mSmoothWindow;
-	private final double mSpikeFixup;
+	private final float mSpikeFixup;
 
 	private final MovingAverage mFilter;
 	private long mLastSpikeTimestamp;
@@ -32,7 +32,7 @@ public class LightSensorFilter {
 	 * @param smoothWindow window size for smoothing (in nanoseconds)
 	 * @param spikeFixup coefficient to use to adjust spikes
 	 */
-	public LightSensorFilter(long smoothWindow, double spikeFixup) {
+	public LightSensorFilter(long smoothWindow, float spikeFixup) {
 		mSmoothWindow = smoothWindow;
 		mSpikeFixup = spikeFixup;
 
@@ -40,13 +40,11 @@ public class LightSensorFilter {
 	}
 
 	public float fixupLuxValue(float lux, long timestamp) {
-		double newLux = lux;
-
 		// In this case the use of exact float comparison is intended.
 		if (lux == HIGH_SPIKE_30K_LUX || lux == HIGH_SPIKE_10K_LUX) {
 			// it goes crazy and shows 30k lux (10k on some devices)
-			newLux = mFilter.getAverage() * mSpikeFixup;
-			Log.d(LOG_TAG, "fixup high spike: " + newLux + " lux");
+			lux = (float) mFilter.getAverage() * mSpikeFixup;
+			Log.d(LOG_TAG, "fixup high spike: " + lux + " lux");
 
 			// sometimes it drops from 30k to zero occasionally,
 			// be ready for fluctuations in the nearest future
@@ -55,18 +53,18 @@ public class LightSensorFilter {
 		} else if (lux == LOW_SPIKE_LUX &&
 				timestamp - mLastSpikeTimestamp < mSmoothWindow * 2) {
 			// reports zero within few ticks after 30k: outlier? out liar!
-			newLux = mFilter.getAverage() / mSpikeFixup;
-			Log.d(LOG_TAG, "fixup low spike:  " + newLux + " lux");
+			lux = (float) mFilter.getAverage() / mSpikeFixup;
+			Log.d(LOG_TAG, "fixup low spike:  " + lux + " lux");
 
 		} else {
 			if (lux == LOW_SPIKE_LUX) {
-				newLux = LOW_REPLACEMENT_LUX;
+				lux = LOW_REPLACEMENT_LUX;
 			}
 
-			mFilter.addValue(newLux, timestamp);
+			mFilter.addValue(lux, timestamp);
 		}
 
-		return (float) newLux;
+		return lux;
 	}
 
 	public long getSmoothWindow() {
